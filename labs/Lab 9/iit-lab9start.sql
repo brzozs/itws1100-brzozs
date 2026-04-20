f2c59420708c488585418c75e91c2e831dcd3786
Sebastian <201484475+brzozs@users.noreply.github.com>
2026-04-20 10:09:57 -0400

    Lab-9
    This is the content of the Lab

--- labs/Lab 9/iit-lab9start.sql
+++ labs/Lab 9/iit-lab9start.sql
@@ -5,7 +5,24 @@ CREATE TABLE `movies` (
    `year` char(4) DEFAULT NULL,
    PRIMARY KEY (`movieid`)
 );
--- insert data into the tables
+
+-- create the actors table
+CREATE TABLE `actors` (
+   `actorid` int(10) unsigned NOT NULL AUTO_INCREMENT,
+   `last_name` varchar(40) NOT NULL,
+   `first_names` varchar(80) NOT NULL,
+   `dob` date NOT NULL,
+   PRIMARY KEY (`actorid`)
+);
+
+-- relationship table between movies and actors
+CREATE TABLE `movie_actors` (
+   `movieid` int(10) unsigned NOT NULL,
+   `actorid` int(10) unsigned NOT NULL,
+   PRIMARY KEY (`movieid`, `actorid`)
+);
+
+-- insert data into movies
 INSERT INTO movies
 VALUES (1, "Elizabeth", "1998"),
    (2, "Black Widow", "2021"),
@@ -16,3 +33,28 @@ VALUES (1, "Elizabeth", "1998"),
       "2001"
    ),
    (5, "Up in the Air", "2009");
+
+-- insert sample actors
+INSERT INTO actors (`actorid`, `last_name`, `first_names`, `dob`)
+VALUES (1, "Blanchett", "Cate", "1969-05-14"),
+   (2, "Johansson", "Scarlett", "1984-11-22"),
+   (3, "Clooney", "George", "1968-05-06"),
+   (4, "Wood", "Elijah", "1981-01-28"),
+   (5, "Nelson", "Tim Blake", "1970-05-11"),
+   (6, "Turturro", "John", "1957-02-28"),
+   (7, "Hunter", "Holly", "1958-03-20"),
+   (8, "McDormand", "Frances", "1957-06-23"),
+   (9, "Downey Jr", "Robert", "1965-04-04");
+
+-- relate actors to movies
+INSERT INTO movie_actors (`movieid`, `actorid`)
+VALUES (1, 1),
+   (2, 2),
+   (2, 9),
+   (3, 3),
+   (3, 5),
+   (3, 6),
+   (3, 7),
+   (4, 4),
+   (5, 3),
+   (5, 8);
--- labs/Lab 9/inclassexample/movies.php
+++ labs/Lab 9/inclassexample/movies.php
@@ -10,7 +10,56 @@
       
 <?php include('includes/menubody.inc.php'); ?>
 
-<p>Build the movie forms and output here.</p>
+<?php
+// Open one database connection for this page.
+$dbOk = false;
+@$db = new mysqli('localhost', 'root', 'root', 'iit');
+
+if ($db->connect_error) {
+  echo '<div class="messages">Could not connect to the database. Error: ';
+  echo $db->connect_errno . ' - ' . $db->connect_error . '</div>';
+} else {
+  $dbOk = true;
+}
+?>
+
+<h3>Movies</h3>
+<table id="actorTable">
+<?php
+if ($dbOk) {
+  $query = 'select * from movies order by title';
+  $result = $db->query($query);
+
+  if ($result) {
+    $numRecords = $result->num_rows;
+
+    echo '<tr><th>Title</th><th>Year</th></tr>';
+    for ($i = 0; $i < $numRecords; $i++) {
+      $record = $result->fetch_assoc();
+      if ($i % 2 == 0) {
+        echo "\n" . '<tr><td>';
+      } else {
+        echo "\n" . '<tr class="odd"><td>';
+      }
+      echo htmlspecialchars($record['title']);
+      echo '</td><td>';
+      echo htmlspecialchars($record['year']);
+      echo '</td></tr>';
+    }
+
+    if ($numRecords == 0) {
+      echo '<tr><td colspan="2">No movies found.</td></tr>';
+    }
+
+    $result->free();
+  } else {
+    echo '<tr><td colspan="2">Could not read movies from database.</td></tr>';
+  }
+
+  $db->close();
+}
+?>
+</table>
 
 <?php include('includes/foot.inc.php'); 
   // footer info and closing tags
