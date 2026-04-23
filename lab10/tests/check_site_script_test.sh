061102a2cc59bfe239bd6b8fa567375f486f5d2d
Sebastian <201484475+brzozs@users.noreply.github.com>
2026-04-23 18:37:32 -0400

    Remove unnecessary files

--- lab10/tests/check_site_script_test.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-repo_root="$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." && pwd)"
-
-bash "$repo_root/lab10/check_site.sh" \
-  "https://brzozsrpi.eastus.cloudapp.azure.com/iit" \
-  "https://brzozsrpi.eastus.cloudapp.azure.com"
